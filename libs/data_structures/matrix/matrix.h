a2a008938a3bbe35905e1ad67a23eddf14f90f95
kndrtsv <kondgenn@gmail.com>
2024-04-03 22:04:43 +0300

    matrix create/delete

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -1,8 +1,23 @@
-//
-// Created by user on 03.04.2024.
-//
-
 #ifndef LWBOP_MATRIX_H
 #define LWBOP_MATRIX_H
+
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+matrix getMemMatrix(int nRows, int nCols);
+
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+void freeMemMatrix(matrix *m);
+
+void freeMemMatrices(matrix *ms, int nMatrices);
 
 #endif //LWBOP_MATRIX_H
