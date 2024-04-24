48546f678be9d7172d56e75df3a5f1daeb558734
kndrtsv <kondgenn@gmail.com>
2024-04-24 23:18:25 +0300

    stringTasks task1

---
+++ libs/string_/tasks/stringTasks.c
@@ -0,0 +1,13 @@
+#include "stringTasks.h"
+
+//task 1
+
+char *getEndOfString(char *s) {
+    return s + strlen_(s) - 1;
+}
+
+void removeNonLetters(char *s) {
+    char *endSource = getEndOfString(s);
+    char *destination = copyIf(s, endSource, s, isgraph);
+    *destination = '\0';
+}
---
+++ libs/string_/tasks/stringTasks.h
@@ -0,0 +1,7 @@
+#ifndef LWBOP_STRINGTASKS_H
+#define LWBOP_STRINGTASKS_H
+
+#include "C:\Users\user\CLionProjects\lwbop\libs\string_\string_.h"
+#include <ctype.h>
+
+#endif //LWBOP_STRINGTASKS_H
