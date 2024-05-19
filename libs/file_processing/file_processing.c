54cd639b0e5dee3e7063221ef03f3687f5f169e9
kndrtsv <kondgenn@gmail.com>
2024-05-19 22:07:15 +0300

    file_processing task1

---
+++ libs/file_processing/file_processing.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "../data_structures/matrix/matrix.h"
+
+//task 1
+
+matrix readSquareMatrixFromFile(FILE *file, int n) {
+    matrix m = getMemMatrix(n, n);
+
+    for (int i = 0; i < m.nRows; i++)
+        for (int j = 0; j < m.nCols; j++) {
+            fscanf(file, "%d", &m.values[i][j]);
+        }
+
+    return m;
+}
+
+void writeSquareMatrixToFile(FILE *f, matrix m) {
+    fprintf(f, "%d\n", m.nRows);
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++)
+            fprintf(f, "%d ", m.values[i][j]);
+        fprintf(f, "\n");
+    }
+}
+
+void transposeMatrices(char *readFile, char *writeFile) {
+    FILE *read = fopen(readFile, "r");
+    FILE *write = fopen(writeFile, "w");
+
+    int n;
+    while (fscanf(read, "%d", &n) != EOF) {
+        matrix m = readSquareMatrixFromFile(read, n);
+        transposeMatrix(&m);
+        writeSquareMatrixToFile(write, m);
+    }
+
+    fclose(read);
+    fclose(write);
+}
---
+++ libs/file_processing/file_processing.h
@@ -0,0 +1,9 @@
+#ifndef LWBOP_FILE_PROCESSING_H
+#define LWBOP_FILE_PROCESSING_H
+
+#include <stdio.h>
+#include "../data_structures/matrix/matrix.h"
+
+void transposeMatrices(char *readFile, char *writeFile);
+
+#endif //LWBOP_FILE_PROCESSING_H
